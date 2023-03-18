41812acbdcf69f2b297c03bfcfe52928c67a8c60
SuryaaAulia <suryaaulia489@gmail.com>
2023-03-19 01:42:03 +0700

    menambahkan class DataGeneric

--- TP_KPL_MOD5_1302210084/Program.cs
+++ TP_KPL_MOD5_1302210084/Program.cs
@@ -1,5 +1,8 @@
 ﻿using TP_KPL_MOD5_1302210084;
 
-String Nama = Console.ReadLine(); ;
+String Nama = Console.ReadLine();
 HaloGeneric hg = new HaloGeneric();
 hg.SapaUser(Nama);
+
+DataGeneric<String> dg = new DataGeneric<String>("1302210084");
+dg.PrintData();
